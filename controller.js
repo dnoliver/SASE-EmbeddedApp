14437cc272e5e69a12cb23b783638f348064d335
Nicolas Oliver <dario.n.oliver@intel.com>
2016-08-03 17:53:23 -0300

    changes into main.js and controller.js

--- controller.js
+++ controller.js
@@ -1,62 +1,72 @@
 var os = require('os');
 var dns = require('dns');
 
+/**
+ * Controller Class
+ * @constructor
+ */
 function Controller() {
-  this.props = {};
-  // _id
-  this.set('_id', '1234');
-  // app/timestamp
-  this.set('app/timestamp', Date.now());
-  // app/host
-  dns.lookup(os.hostname(), function (err, address, family) {
-    this.set('app/host', address);
-  }.bind(this));
-  // app/type
-  this.set('app/type', 'embedded');
-  // app/message/in
-  this.add('app/channels', {
-    topic: 'app/message/in',
-    qos: 0,
-    type: 'string',
-    description: 'Message Reception',
-    direction: 'in'
-  });
-  // app/message/out
-  this.add('app/channels', {
-    topic: 'app/message/out',
-    qos: 0,
-    type: 'string',
-    description: 'Message Delivery',
-    direction: 'out'
-  });
+    this.props = {};
+    // _id
+    this.set('_id', '1234');
+    // app/timestamp
+    this.set('app/timestamp', Date.now());
+    // app/host
+    var networkInterfaces = os.networkInterfaces();
+    // harcoded wlan0 interface
+    this.set('app/host', networkInterfaces['wlan0'][0].address);
+    /**
+     * Can also use dns.lookup like this:
+     *
+     * dns.lookup(os.hostname(), function (err, address, family) {
+     *   this.set('app/host', address);
+     * }.bind(this));
+     */
+    // app/type
+    this.set('app/type', 'embedded');
 }
 
+/**
+ * Get a Controller property
+ * @param {string|function} [property]
+ * @param {function} callback
+ */
 Controller.prototype.get = function (property, callback) {
-  if (typeof property === 'function') {
-    callback = property;
-    callback(null, this.props);
-    return;
-  }
+    if (typeof property === 'function') {
+        callback = property;
+        callback(null, this.props);
+        return;
+    }
 
-  switch (property) {
-  case 'app/host':
-    dns.lookup(os.hostname(), callback);
-    break;
-  default:
-    callback(null, this.props[property]);
-  }
+    switch (property) {
+    case 'app/host':
+        dns.lookup(os.hostname(), callback);
+        break;
+    default:
+        callback(null, this.props[property]);
+    }
 };
 
+/**
+ * Set a Controller property
+ * @param {string} property
+ * qparam {object} value
+ */
 Controller.prototype.set = function (property, value) {
-  this.props[property] = value;
+    this.props[property] = value;
 };
 
+/**
+ * Add a value to a Controller property
+ * @param {string} property
+ * qparam {object} value
+ */
 Controller.prototype.add = function (property, value) {
-  if (!this.props[property]) {
-    this.props[property] = [];
-  }
-  
-  this.props[property].push(value);
+    if (!this.props[property]) {
+        this.props[property] = [];
+    }
+
+    this.props[property].push(value);
 };
 
 module.exports = Controller;
--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var express = require("express");
 var RED = require("node-red");
 var mosca = require("mosca");
 var util = require('util');
+var path = require('path');
+var os = require('os');
 var Controller = require('./controller');
 
 /**
@@ -13,6 +15,10 @@ var controller = new Controller();
 /**
  * Express Server Creation
  */
+
+var ExpressAppSettings = {
+    port: 8000
+};
 
 // Create an Express app
 var app = express();
@@ -25,32 +31,32 @@ var server = http.createServer(app);
  */
 
 var MoscaServerSettings = {
-  port: 1883,
-  http: {
-    port: 8001
-  }
+    port: 1884, // tsl port, provide Mqtt Standard support
+    http: {
+        port: 8001 // http port, provide Mqtt over Websockets support
+    }
 };
 
 var MoscaServer = new mosca.Server(MoscaServerSettings);
 
 // fired when the mqtt server is ready
 MoscaServer.on('ready', function () {
-  console.log('[MoscaServer] up and running in port', MoscaServerSettings.port);
-  console.log('[MoscaServer] http port is', MoscaServerSettings.http.port);
+    console.log('[MoscaServer] up and running in port', MoscaServerSettings.port);
+    console.log('[MoscaServer] http port is', MoscaServerSettings.http.port);
 });
 
 // fired when a message is published
 MoscaServer.on('published', function (packet) {
-  console.log('[MoscaServer]', 'Published', packet.topic);
+    console.log('[MoscaServer]', 'Published', packet.topic);
 });
 // fired when a client connects
 MoscaServer.on('clientConnected', function (client) {
-  console.log('[MoscaServer]', 'Client Connected:', client.id);
+    console.log('[MoscaServer]', 'Client Connected:', client.id);
 });
 
 // fired when a client disconnects
 MoscaServer.on('clientDisconnected', function (client) {
-  console.log('[MoscaServer]', 'Client Disconnected:', client.id);
+    console.log('[MoscaServer]', 'Client Disconnected:', client.id);
 });
 
 /**
@@ -59,13 +65,14 @@ MoscaServer.on('clientDisconnected', function (client) {
 
 // Node-RED Settings
 var settings = {
-  httpAdminRoot: "/admin",
-  httpNodeRoot: "/api",
-  userDir: "./.nodered",
-  flowFile: "flows.json",
-  functionGlobalContext: {
-    Controller: controller
-  }
+    httpAdminRoot: "/admin",
+    httpNodeRoot: "/api",
+    userDir: "./.node-red",
+    flowFile: "flows.json",
+    functionGlobalContext: {
+        Controller: controller,
+        UpmBuzzer: require('jsupm_buzzer')
+    }
 };
 
 // Initialise the runtime with a server and settings
@@ -77,13 +84,13 @@ app.use(settings.httpAdminRoot, RED.httpAdmin);
 // Serve the http nodes UI from /api
 app.use(settings.httpNodeRoot, RED.httpNode);
 
-server.listen(8000);
+server.listen(ExpressAppSettings.port);
 
 // Start the runtime
 RED.start();
 
-controller.set('app/name', 'SASE-EmbeddedApp');
-controller.set('app/port', 8000);
+controller.set('app/name', os.hostname());
+controller.set('app/port', ExpressAppSettings.port);
 controller.set('app/api', settings.httpNodeRoot);
 controller.set('app/broker/port', MoscaServerSettings.port);
 controller.set('app/broker/http/port', MoscaServerSettings.http.port);
